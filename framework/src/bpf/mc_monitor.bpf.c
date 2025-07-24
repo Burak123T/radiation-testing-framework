f6afbf71fc800adf8171f99a8337912ba228f65c
Burak123T <burak-9@live.dk>
2025-07-24 21:55:27 +0200

    attempting to add __data_loc decoding

--- framework/src/bpf/mc_monitor.bpf.c
+++ framework/src/bpf/mc_monitor.bpf.c
@@ -8,6 +8,24 @@ struct {
     __uint(max_entries, 4096);
 } mc_events SEC(".maps");
 
+/**
+ * Helper function to decode the various "__data_loc" fields for the Memory Controller tracepoint.
+ * Help found thanks to Github issue: https://github.com/bpftrace/bpftrace/issues/385
+ */
+void decode_mc_data_loc(mc_event_t *event){
+    unsigned short offset = event->__data_loc_msg & 0xFFFF;
+    unsigned short length = event->__data_loc_msg >> 16;
+    bpf_probe_read_kernel_str(event->msg, length, (char*)event + offset);
+
+    unsigned short offset = event->__data_loc_label & 0xFFFF;
+    unsigned short length = event->__data_loc_label >> 16;
+    bpf_probe_read_kernel_str(event->label, length, (char*)event + offset);
+
+    unsigned short offset = event->__data_loc_driver_detail & 0xFFFF;
+    unsigned short length = event->__data_loc_driver_detail >> 16;
+    bpf_probe_read_kernel_str(event->driver_detail, length, (char*)event + offset);
+}
+
 SEC("tracepoint/ras/mc_event")
 int trace_mc_event(struct trace_event_raw_mc_event *ctx) {
     mc_event_t *event;
